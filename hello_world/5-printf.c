017389c13e9df6018def2369594a2d9422237d60
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-14 11:05:50 +0100

    fix betty

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * return zero if sucess
+ * Return: Always 0 (Sucess)
  */
 
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
