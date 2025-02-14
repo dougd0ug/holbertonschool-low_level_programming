d95ea8695c110990e3ba3c1a60500a16303793ff
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-14 11:24:15 +0100

    add long

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	printf("Size of a char: %lu bytes\n", sizeof(char));
 	printf("Size of an int: %lu bytes\n", sizeof(int));
-	printf("Size of a long int: %lu bytes\n", sizeof(int *));
-	printf("Size of a long long int: %lu bytes\n", sizeof(int **));
+	printf("Size of a long int: %lu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
 	printf("Size of a float: %lu bytes \n", sizeof(float));
 	return (0);
 }
