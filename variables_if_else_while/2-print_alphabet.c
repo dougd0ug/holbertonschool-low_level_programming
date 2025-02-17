5ed28154fd4970ebcd7117e08f189ba34942f657
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-17 10:44:43 +0100

    prints alphabet xith putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabet
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	long char ch = 'abcdefghijklmnopqrstuvwxyz\n';
+	putchar(ch);
+	return (0);
+
+}
