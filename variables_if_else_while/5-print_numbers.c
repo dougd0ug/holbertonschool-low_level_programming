002f0e9d6999586684857b88f8bafad0c25d7011
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-17 13:41:59 +0100

    print num

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int alph;
+
+	for (alph = 0; alph <= 10; alph++)
+	{
+
+		putchar(alph);
+
+	}
+	putchar('\n');
+	return (0);
+}
