65ea7922394211143db51a3e57be1bccc4b2e064
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-17 11:08:47 +0100

    prints lowercase and uppercase alph with putchar

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	char alph2 = 'A';
+
+	while (alph2 <= 'Z')
+	{
+		putchar(alph2);
+		alph2++
+	}
+	putchar('\n');
+	return (0);
+}
