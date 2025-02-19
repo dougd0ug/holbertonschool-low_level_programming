a0cd73c5a1f2ad5f2f4f60232c01c5f7aff69f74
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-19 09:50:38 +0100

    try with if

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Entry
+ * c - the character we want to check
+ * c: the character we want to check
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
