b48650ed767feee06f9c97e8695f2e0e3a538ad0
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-19 15:15:50 +0100

    add 2 integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Entry
+ * @a: first int
+ * @b: second int
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int add(int a, int b);
+{
+	int result;
+
+	result = (a + b);
+
+	_putchar(result + '0');
+}
