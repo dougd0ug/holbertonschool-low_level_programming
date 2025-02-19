2aadf90fbb437f4877356e9c5b4dc62a014eebb8
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-19 11:26:18 +0100

    print all hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; hour++)
+		{
+			_putchar(hour);
+			_putchar(':');
+			_putchar(min);
+			_putchar('\n');
+		}
+	}
+}
