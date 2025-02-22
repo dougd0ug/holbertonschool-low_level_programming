64ab7fbd62b7ebfd33199be5040b5adb78b79523
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-22 18:56:28 +0100

    try xith digit 1 and 2

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,19 @@
 int main(void)
 {
 	int digit1;
-    int digit2;
-    
-    for(digit1 = 0; digit1 < 9; digit1++)
-    {
-        for(digit2 = digit1 + 1; digit2 < 10; digit2 ++)
-        {
-            putchar((digit1 % 10) + '0');
-            putchar((digit2 % 10) + '0');
-        }
-        while(digit1 != 8 && digit2 !=9)
-        {
-            putchar('\n');
-        }
-    }
-    return (0);
+	int digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+		}
+		while (digit1 != 8 && digit2 != 9)
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
 }
