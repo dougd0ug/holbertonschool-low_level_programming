b53c540bcf73dc2190a07c1f6f6dd118d1d014f0
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-22 18:54:06 +0100

    try with 3 boucles

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,20 @@
 
 int main(void)
 {
-	int alph;
-	int beta;
-
-	for (alph = 0; alph < 90; alph++)
-	{
-
-		while ((alph / 10) ! (alph % 10))
-		{
-			putchar((alph / 10) + '0');
-			putchar((alph % 10) + '0');
-		}
-		
-		
-	
-		
-		if (alph < 90)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+	int digit1;
+    int digit2;
+    
+    for(digit1 = 0; digit1 < 9; digit1++)
+    {
+        for(digit2 = digit1 + 1; digit2 < 10; digit2 ++)
+        {
+            putchar((digit1 % 10) + '0');
+            putchar((digit2 % 10) + '0');
+        }
+        while(digit1 != 8 && digit2 !=9)
+        {
+            putchar('\n');
+        }
+    }
+    return (0);
 }
