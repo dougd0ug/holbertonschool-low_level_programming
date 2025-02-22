e82ccc9a438e0f3f3a2c72c9ce573058c16cae3a
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-22 19:14:13 +0100

    good loop

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -17,13 +17,14 @@ int main(void)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
+
+				if (digit1 == 8 && digit2 == 9)
+				{
+					continue;
+				}
+			putchar(',');
+			putchar(' ');
 		}
-		if (digit1 == 8 && digit2 == 9)
-		{
-			continue;
-		}
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
