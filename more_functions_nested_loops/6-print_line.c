07cf8cce431c133dd95c4655904a8a099e9f8493
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-24 11:29:37 +0100

    print lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - Entry
+ * @n: number of lines
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (j = 0; j < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
