7e2e1c6cd42cce8f2fd9dc121dce53888a346b1f
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-24 15:16:21 +0100

    fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		} else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		} else
+		{
+			printf("%d", i);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
