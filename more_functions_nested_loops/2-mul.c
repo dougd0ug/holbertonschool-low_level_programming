852b499d7b96d9a7c38f5577510cebd59dbee03f
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-24 10:45:16 +0100

    multiply 2 numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry
+ * @a: the digit we want to check
+ * @b: the second digit
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
