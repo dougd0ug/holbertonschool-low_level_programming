693cbf241c132fc734a85d5d93f81aee9c3d3824
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-24 14:33:11 +0100

    print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry
+ * @n: number of lines
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
