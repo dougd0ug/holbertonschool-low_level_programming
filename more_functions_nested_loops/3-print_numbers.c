40f22cbcf1bd8c2a88bcd99978ca3078399cf989
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-24 10:49:48 +0100

    print all 9 numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
