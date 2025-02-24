a3d01a649f9e3a23d116222e7fccbf6301be8255
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-24 10:11:29 +0100

    try with 2 if to compare an uppercase or else

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry
+ * @c: the character we want to check
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _isupper(int c)
+{
+	if (c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
