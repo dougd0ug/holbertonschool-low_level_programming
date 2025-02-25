bd2d7e0efc4603c2ca6b3b95fcb16ca194d39441
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-25 10:59:34 +0100

    update a pointer to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry
+ * @n: parameter we want to update
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
