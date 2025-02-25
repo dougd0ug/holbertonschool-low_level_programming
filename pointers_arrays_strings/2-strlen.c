c8e5f9e8c767970cedf56a3990683de9fe223e6d
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-25 16:12:33 +0100

    number of strings

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry
+ * @s: parameter we want to check
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; length[i]; length++)
+	{
+		return (length);
+	}
+}
