b2c6257d5d28ae7a18942b77aa0ad3b17102432a
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-26 16:16:32 +0100

    sdfsdf

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -22,9 +22,9 @@ void print_array(int *a, int n)
 			printf("%d", a[i]);
 		} else
 		{
-			printf("%d", a[i]);
 			_putchar(',');
 			_putchar(' ');
+			printf("%d", a[i]);
 		}
 	}
 	_putchar('\n');
