34a7be93f06cb1353e9c2ba9e7cd1dcaf8b3f05d
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-26 09:37:45 +0100

    prints a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Entry
+ * @str: parameter we want to puts
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	_putchar(str[i]);
+
+}
