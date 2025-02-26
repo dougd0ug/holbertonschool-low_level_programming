4c58a2a2ec83c06ca6ecf01f9cd24f9f3840f1ea
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-26 14:19:02 +0100

    reverse a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry
+ * @s: parameter we want to puts
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void rev_string(char *s)
+{
+	int length;
+	int rev;
+	int string;
+
+	for (length = 0; s[length]; length++)
+	{
+	}
+
+	length--;
+
+	for (rev = length; rev >= 0; rev--)
+	{
+		return (string[rev]);
+	}
+	_putchar('\n');
+}
