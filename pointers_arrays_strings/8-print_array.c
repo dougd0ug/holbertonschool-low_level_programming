47622cfafd8396db1a949e3127df9c21555f4906
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-26 16:00:33 +0100

    print a character

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_array - print array
+ * @a: array
+ * @n: number of repetitions
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		_putchar(',');
+		_putchar(' ');
+
+		if (i == n)
+		{
+			_putchar(a[i]);
+		}
+	}
+	_putchar('\n');
+}
