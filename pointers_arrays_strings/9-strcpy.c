a24d722167d897b5610fabeca875d794f755cb35
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-27 14:28:38 +0100

    switch strings

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - print half of a string
+ * @dest: parameter we want to puts
+ * @src: parameter we want to switch
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		src[i] = dest[i];
+	}
+
+	return (*dest);
+}
