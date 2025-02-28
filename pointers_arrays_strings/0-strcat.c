c0703f0e74a8ef0e6fd78aa6113fdb26dd98804f
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-28 10:23:51 +0100

    contznante 2strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -15,13 +15,13 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	for (i = 0; dest[i]; i++)
-	{		
+	{
 	}
-	
+
 	for (j = 0; src[j]; j++)
 	{
 		dest[i + j] = src[j];
 	}
 	dest[i + j] = '\0';
-	return(dest);
+	return (dest);
 }
