9e4725f43d0c54961d2a1b4ca61a215b60bf28ac
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-28 10:05:09 +0100

    copycat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcat - print half of a string
+ * @dest: parameter we want to puts
+ * @src: parameter we want to switch
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int *i;
+
+	i = dest[] +src[];
+	dest = i;
+	return (dest);
+}
