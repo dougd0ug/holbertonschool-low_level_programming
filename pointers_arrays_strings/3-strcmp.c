40aea8d3cf34ae644bc4d79509831cf48eb445b9
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-28 14:15:47 +0100

    file

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Write a function that copies a string
+ * @dest: parameter we want to puts
+ * @src: parameter we want to switch
+ * @n: number of repetitions
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int copy;
+
+	for (copy = 0; src[copy] != '\0' && copy < n; copy++)
+	{
+		dest[copy] = src[copy];
+	}
+
+	for (;copy < n; copy++)
+	{
+		dest[copy] = '\0';
+	}
+	return (dest);
+}
