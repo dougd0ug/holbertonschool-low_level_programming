e06e2d65a820a6f12f4bc45d97f4c15ffb9db640
dougd0ug <dorine.sgv@hotmail.fr>
2025-02-28 11:03:13 +0100

    copy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncat - Write a function that copies a string
+ * @dest: parameter we want to puts
+ * @src: parameter we want to switch
+ * @n: number of repetitions
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int length;
+	int copy;
+
+	for (length = 0; dest[length]; length++)
+	{
+	}
+	
+	for (copy = 0; copy <= length; length++)
+	{
+		src[n] = dest[length]
+	}
+	return(src);
+}
