f41a066a20071ead743ecf852eec974250bebe2f
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-02 23:59:03 +0100

    last try

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -3,11 +3,7 @@
 /**
  * leet - change letters to digits
  * @l: parameter we want to puts
- * parameter we want to switch
- * _putchar - writes the character c to stdout
- *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 
 char *leet(char *l)
