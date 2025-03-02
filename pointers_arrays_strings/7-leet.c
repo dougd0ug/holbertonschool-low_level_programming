524f22060aacd1578405258ea60c5933af0dd2b1
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-02 23:59:37 +0100

    last try

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ char *leet(char *l)
 	int j;
 	char lettre[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char number[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	
+
 	for (i = 0; l[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
