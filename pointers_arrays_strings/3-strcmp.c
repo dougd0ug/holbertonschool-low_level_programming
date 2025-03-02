4ec1ad5a2546d73676e978099800dc806ac8ac73
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-02 20:15:34 +0100

    try with while

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,20 +12,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int comp = 0;
+	int i = 0;
+	int comp;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
-		for (; s2[i] != '\0'; i++)
+		i++;
+		if (s1[i] != s2[i])
 		{
-			if (s1[i] > s2[i] || s1[i] < s2[i])
-			{
-				comp = (s1[i] - s2[i]);
-			} else
-			{
-				comp = 0;
-			}
+			comp = (s1[i] - s2[i]);
 		}
 	}
 	return (comp);
