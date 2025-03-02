c92146c5c4a95d7a8b1bfb6bec6af8a6da376315
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-02 20:22:23 +0100

    reverse an array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - Write a function that reverses the content
+ * @a: parameter we want to puts
+ * @n: parameter we want to switch
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int rev;
+
+	for (i = n; i > n; i--)
+	{
+		rev = a[i];
+	}
+	return (rev);
+}
