b88dabf1fdd4a54c61672d8386e28fdabd8aa3ef
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-02 21:12:46 +0100

    remplace

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,13 +14,13 @@ char *cap_string(char *l)
 {
 	char symb[14] = { 32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 	int i;
-	int j = 0;
+	int j;
 
 	for (i = 0; l[i] != '\0'; i++)
 	{
 		if (l[i] >= 'a' && l[i] <= 'z')
 		{
-			while (j < 14)
+			for (j = 0; j < 14; j++)
 			{
 				if (l[i - 1] == symb[j])
 				{
