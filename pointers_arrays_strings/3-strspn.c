d9ec34fb439ef9f1c500cf931a7281881c69af4f
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-03 14:30:42 +0100

    compare chracter

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strspn - Write a function that gets the length of a prefix substring
+ * @s: string to be scanned
+ * @accept: string containing the characters to match
+ *
+ * Return: Always 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int result = 0;
+
+	while (s[i])
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				result++;
+			}
+		}
+		i++;
+	}
+	return (result);
+}
