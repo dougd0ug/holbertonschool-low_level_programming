edc9c956665494160e871848965c55aa6b8850e1
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-04 09:47:11 +0100

    prints the comapraison

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strpbrk - Write a function that searches a string for any of a set of bytes
+ * @s: string to be scanned
+ * @accept: string containing the characters to match
+ *
+ * Return: Always 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+	int result;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		result = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				result = 1;
+				break;
+				return (s);
+			}
+		}
+
+		if (result == 0)
+		{
+			break;
+		}
+	}
+	return (0);
+}
