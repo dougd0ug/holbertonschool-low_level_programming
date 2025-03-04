16c76ce78adc6fcbf940d13687dcc04fce01ad0b
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-04 19:37:50 +0100

    add conditions

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -11,12 +11,11 @@
 void _puts_recursion(char *s)
 {
 	int i = 0;
-	
-	
+
 	if (i < s[i])
 	{
 		_putchar(s[i]);
-		_puts_recursion(&s[i]);
+		_puts_recursion(&s[i + 1]);
 	} else
 	{
 		_putchar('\n');
