055ebd83f1d94027b12d5d421b1741e1d28e8581
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-05 10:27:53 +0100

    factorial recuvrsive

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - Write a function that returns the factorial
+ * @n: digit we want to convert in factorial
+ *
+ * Return: Always 0
+ */
+
+int factorial(int n)
+{
+	if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
