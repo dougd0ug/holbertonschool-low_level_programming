8c8f904a3ef1f5500b1e801e1c0b8c604d8fd33f
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-05 09:43:07 +0100

    print in recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -12,12 +12,12 @@ void _print_rev_recursion(char *s)
 {
 	int i = 0;
 
-	if (i < s[i] && s[i] != '\0')
+	if (s[i] != '\0')
 	{
 		_print_rev_recursion(&s[i + 1]);
+		_putchar(s[i]);
 	} else
 	{
-		_putchar(s[i]);
 		_putchar('\n');
 	}
 }
