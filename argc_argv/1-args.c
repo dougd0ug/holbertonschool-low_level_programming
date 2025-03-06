0e2d1102ab857965f52fe86737dd7f9771ccdd0b
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-06 23:32:46 +0100

    print numbers of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - Entry
+ *@argc: int
+ *@argv: array
+ *Return: squared root
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
