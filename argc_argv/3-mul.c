8430ec51d47b8aff210b4af514f03bee3b9dc502
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-07 13:34:23 +0100

    fix lines

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,15 +11,15 @@
 int main(int argc, char *argv[])
 {
 	int result;
-	int nb1 = atoi(argv[1]);
-	int nb2 = atoi(argv[2]);
-
-	if (argc > 5 || argc < 3)
+	
+	if (argc < 3 || argc > 5)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
+	
+	int nb1 = atoi(argv[1]);
+	int nb2 = atoi(argv[2]);
 
 	result = (nb1 * nb2);
 	printf("%d\n", result);
