69d3a17766c4d7eee92f5a99c921b68b2bdc466b
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-07 13:31:38 +0100

    multiply

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	int nb1 = atoi(argv[1]);
 	int nb2 = atoi(argv[2]);
 
-	if (argc > 3 || argc < 3)
+	if (argc > 5 || argc < 3)
 	{
 		printf("Error");
 		return (1);
