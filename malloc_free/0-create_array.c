f821eda5084f50aabee61ddd5412c10582d2bd18
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-10 10:43:25 +0100

    betty style and create an array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,17 +14,23 @@ char *create_array(unsigned int size, char c)
 {
 	char *tab;
 	unsigned int i;
-	
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	
+
 	tab = malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
 		tab[i] = c;
 	}
+
+	if (tab == NULL)
+	{
+		return (NULL);
+	}
+
 	return (tab);
 }
