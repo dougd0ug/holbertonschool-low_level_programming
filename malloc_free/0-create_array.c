1d722e926e673f31d1f47c552d5e0e7c33244bde
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-10 10:40:49 +0100

    creat array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,14 +12,15 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *tab;
+	unsigned int i;
+	
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
-	char *tab;
+	
 	tab = malloc(size * sizeof(char));
-	int i;
 
 	for (i = 0; i < size; i++)
 	{
