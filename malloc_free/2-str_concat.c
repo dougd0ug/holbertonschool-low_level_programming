06af770c3c51f66641a62648e4b4ab3a79989068
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-10 14:34:21 +0100

    concatenante

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,39 +11,38 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int j;
-	int k;
+	int i, j, k;
 	char *copy;
 
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
-
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 	}
-
 	copy = malloc(sizeof(char) * j + 1 * i + 1);
-
 	if (copy == NULL)
 	{
 		return (NULL);
 	}
-
-	for (k = 0; k < copy[i + j]; i++)
+	i = 0;
+	j = 0;
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		copy[i + j] = s1[i] + s2[j];
+		copy[i] = s1[i];
+	}
+
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		copy[i] = s2[j];
 	}
 	return (copy);
 }
