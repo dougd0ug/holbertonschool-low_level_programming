b37f245f418af79a1ced7d4149c81cbca7daf934
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-10 15:38:38 +0100

    double array

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *alloc_grid - Write a function that returns a pointer
+ * to a 2 dimensional array of integers
+ * @width: largeur
+ * @height: hauteur
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int *copy;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	copy = malloc(sizeof(int) * (height + width));
+
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	height = 0;
+	width = 0;
+	return (copy);
+}
