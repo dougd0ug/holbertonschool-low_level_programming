0ac1364cfcc3357bf95c4d36649643692d58dbad
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-11 13:39:34 +0100

    create array of integers

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *array_range - Write a function that creates an array of integers
+ * @min: array we want to concatenate
+ * @max: array we want to concatenate
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *array;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	array = malloc(max - min + 1);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; min < max; i++)
+	{
+		array[min] += 1;
+	}
+	return (array);
+}
