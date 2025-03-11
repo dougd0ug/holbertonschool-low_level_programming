0f8662016d243188176d22e59c9c7dd70632b8d4
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-11 11:56:30 +0100

    calloc function

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - Write a function that allocates memory for an array, using malloc
+ * @nmemb: array we want to concatenate
+ * @size: array we want to concatenate
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *result;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	result = malloc(nmemb * size);
+
+	if (malloc == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		result = 0;
+	}
+	return (result);
+}
