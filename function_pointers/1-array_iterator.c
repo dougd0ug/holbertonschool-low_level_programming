caf770c76a5097b4ec316b516b3e51aa89220830
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-17 10:51:27 +0100

    iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - Write a function that executes a
+ * function given as a parameter on each
+ * element of an array
+ * @array: tableau
+ * @size: taille tableau
+ * @action: different ways
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || size == 0)
+	{
+		return;
+	}	
+
+	for (i = 0; array[i]; i++)
+	{
+		action(array[i]);
+	}	
+}
