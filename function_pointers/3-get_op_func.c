03b783ab64be96459e87fd7abf3627695cedb8d3
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-18 11:03:55 +0100

    bettyyyyyyyyyy

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,10 +21,10 @@ int (*get_op_func(char *s))(int, int)
 	int i = 0;
 
 	while (ops[i].op != NULL)
-    {
-        if (strcmp(s, ops[i].op) == 0)
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
