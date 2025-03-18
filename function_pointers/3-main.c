9a0c2a5dc3db3255235a75381c1a07b11be79a6a
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-18 10:42:55 +0100

    main

---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <strings.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - calculate
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: result
+ */
+
+int main(int argc, char **argv)
+{
+	int num1;
+	int num2;
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if ((argv[2] == '%' ||  argv[2] == '/') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", result);
+
+	return (result);
+}
