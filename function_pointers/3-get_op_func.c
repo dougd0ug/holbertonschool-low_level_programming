e0e5ab53bc25c697781c1c53211e685f3697dc58
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-18 10:28:26 +0100

    3 files

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * *get_op_func - have the good operator
+ * @s: operator
+ * Return: 99 or the good operator
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	if (s != "+" || s != "-" || s != "*" || s != "%" || s != "/")
+	{
+		return (NULL);
+	}
+
+	while (ops[i] && s != ops[i].op)
+	{
+		i++
+	}
+	return (ops[i].f);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - add two integers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub two integers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul two integers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div two integers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo two integers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
