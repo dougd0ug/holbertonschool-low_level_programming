e76d0eb7e54b5c2def8b9ae21aefc492f25690f9
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-19 20:08:22 +0100

    header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,20 @@
 #ifndef VAR
 #define VAR
 
+typedef struct op
+{
+	char *form;
+	void (*f)(va_list);
+} op_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void aff_c(va_list aff_list);
+void aff_i(va_list aff_list);
+void aff_f(va_list aff_list);
+void aff_s(va_list aff_list);
 
 #endif
