3f09e5c13846902d0877a7a51a6f68e61cd8f719
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-31 16:09:49 +0200

    add a free after malloc and strdup

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -34,8 +34,10 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node->str == NULL)
 	{
 		free(new_node->str);
+		free(new_node);
 		return (NULL);
 	}
+
 	new_node->len = i;
 	new_node->next = *head;
 
