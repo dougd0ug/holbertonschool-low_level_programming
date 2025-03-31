40629ac64e9a574a199b2f4ed837136a6391c061
dougd0ug <dorine.sgv@hotmail.fr>
2025-03-31 13:52:40 +0200

    print all elements of a list, add a while loop

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,17 +3,34 @@
 #include <string.h>
 #include <stdio.h>
 
+
 /**
- * 
- * 
+ * print_list - print all elements of a list
+ * @h: list
+ * Return: length of the list
  */
 
 size_t print_list(const list_t *h)
 {
-	if (h->str == NULL)
+	int length = 0;
+
+	if (h == NULL)
 	{
-		printf("[0] (nil)");
+		return (-1);
 	}
 
-	return(h->len);
+	while (h->next)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		length++;
+		h = h->next;
+	}
+	return (length);
 }
