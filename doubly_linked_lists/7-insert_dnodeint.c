6f3524e835bd2e6649d9eb883498cc61c8da36c3
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-02 14:19:47 +0200

    change the i

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -37,8 +37,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			tmp->next = new_node;
 			new_node->prev = tmp;
 		}
-		i++;
 		*h = (*h)->next;
+		i++;
 	}
 	else
 	{
