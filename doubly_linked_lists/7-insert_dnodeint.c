7a9b346126aa212e21f124c3bf8064d258e6ad6f
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-02 14:52:08 +0200

    fix wrong if

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,8 +15,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *new_node;
-	dlistint_t *tmp;
+	dlistint_t *new_node, *tmp;
 
 	new_node = malloc(sizeof(dlistint_t));
 
@@ -30,20 +29,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = NULL;
 	tmp = *h;
 
-	if (h != NULL)
+	if (*h == NULL && idx > 0)
+		free(new_node);
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	while (tmp != NULL && i < idx - 1)
 	{
-		if (i == idx)
-		{
-			tmp->next = new_node;
-			new_node->prev = tmp;
-		}
-		*h = (*h)->next;
+		tmp = tmp->next;
 		i++;
 	}
-	else
+	if (tmp == NULL)
 	{
-		*h = new_node;
+		free(new_node);
 		return (NULL);
 	}
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+	new_node->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = new_node;
+
+	tmp->next = new_node;
+	new_node->prev = tmp;
 	return (new_node);
 }
