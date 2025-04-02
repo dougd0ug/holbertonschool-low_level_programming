0a97e47d6c00bfa7c60e00f1e8139f071eb7c2aa
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-02 10:00:52 +0200

    fix free problem

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -37,7 +37,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	tmp = *head;
 	if (new_node->str == NULL)
 	{
-		free(new_node->str);
+		free(new_node);
 		return (NULL);
 	}
 	new_node->len = i;
