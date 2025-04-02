affd2a70090449f20c684106ff6fb52ab750c79e
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-02 14:53:50 +0200

    fix if

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -30,10 +30,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp = *h;
 
 	if (*h == NULL && idx > 0)
-		free(new_node);
-		return (NULL);
+			free(new_node);
+			return (NULL);
 	if (idx == 0)
-		return (add_dnodeint(h, n));
+			return (add_dnodeint(h, n));
 	while (tmp != NULL && i < idx - 1)
 	{
 		tmp = tmp->next;
@@ -45,7 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
+			return (add_dnodeint_end(h, n));
 	new_node->next = tmp->next;
 	if (tmp->next != NULL)
 		tmp->next->prev = new_node;
