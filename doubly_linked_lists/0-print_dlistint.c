0d2fc262aed861fe02fb7e843bf134ede710436e
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-02 10:43:06 +0200

    print all elements of a list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+* print_dlistint - print all elements of a list
+* @h: list
+* Return: length of the list
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int length = 0;
+
+	if (h == NULL)
+	{
+		return (length);
+	}
+
+	while (h != NULL)
+	{
+		printf("%d", length);
+		length++;
+		h = h->next;
+	}
+	return (length);
+}
