ac3b5006e4e195c308cd117811c16382d7f0d7ca
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-02 11:03:41 +0200

    fix if head is NULL

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -18,15 +18,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (new_node == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
-	
 	new_node->n = n;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
