a7cb4189f98d123887417c408a815900a9833372
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-04 13:00:48 +0200

    size-t problems fix

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef FILE_HEADER
 #define FILE_HEADER
+#include <stddef.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
